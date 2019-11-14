6fdacd3fba10c2acae202c514ebd574cd827f23d
joshnevin <36230243+joshnevin@users.noreply.github.com>
2019-11-14 15:22:24 +0000

    Add files via upload

--- gradfunc.m
+++ gradfunc.m
@@ -1,19 +1,13 @@
 function grad = gradfunc(xstar, X, k1, k2, al)
 
-%Ktest = sqexp(xstar,X,k1,k2); 
-
-%Ktest = sum(Ktest,2);
-
 K = zeros(size(X,1),1); 
 for i = 1:size(X,1)
 
-    K(i) = k2*exp((-norm(xstar - X(i,:))^2)/(2*k1)); 
-
+    K(i) = (k2^2)*exp((-norm(xstar - X(i,:))^2)/(2*(k1^2)));  
+    
 end
 
-%grad = (k2/k1)*(X - xstar)'*K*al'; 
-
-grad = (1/k1)*(X - xstar)'*K*al'; 
+grad = (1/(k1^2))*(X - xstar)'*K*al'; 
 
 grad = sum(grad,2);
 
